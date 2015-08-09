258a79cd2c0d49ee884e24dececae2f9841374a0
Tyler Machen <tylermachen@gmail.com>
2015-08-08 22:02:45 -0400

    complete spotify api methods

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -1,54 +1,28 @@
 require 'json'
 require 'open-uri'
+require 'pry'
 
 class SpotifyChart
-
-  BASE_URL = "?"
+  BASE_URL = "http://charts.spotify.com/api/tracks/most_streamed"
 
   def get_url(region)
-    # return a string that is the BASE_URL/region/weekly/latest
+    "#{BASE_URL}/#{region}/weekly/latest"
   end
 
   def get_json(url)
-    # load json given a url here
-    # refer to the references if you have questions about this
+    JSON.load(open(url))
   end
 
   def get_first_track_info(music_hash)
-    # example music_hash:
-    # {  
-    #   "tracks" => [  
-    #     {  
-    #       "date"         =>"2014-09-14",
-    #       "track_name"   =>"All About That Bass",
-    #       "artist_name"  =>"Meghan Trainor",
-    #       "album_name"   =>"Title"
-    #     },
-    #     {  
-    #       "date"         =>"2014-09-14",
-    #       "track_name"   =>"Break Free",
-    #       "artist_name"  =>"Ariana Grande",
-    #       "album_name"   =>"Break Free"
-    #     }
-    #   ]
-    # }
-
-    # given a hash, like the one above, this method should return a string like:
-    # <track name> by <artist name> from the <album name>
-  
-    # the track name, artist name, and album name should be the first track in the
-    # tracks array
+    track = music_hash["tracks"].first["track_name"]
+    artist = music_hash["tracks"].first["artist_name"]
+    album = music_hash["tracks"].first["album_name"]
+    "#{track} by #{artist} from the album #{album}"
   end
-
 
   def most_streamed(region)
-    # call on #get_url here, where preference is the string 'most_streamed',
-    # and set it equal to a variable
-
-    # call on #get_json here, using the string that get_url returns
-    
-    # finally, call on #get_first_track_info using the 
-    # hash that #get_json returns
+    url = get_url(region)
+    hash = get_json(url)
+    get_first_track_info(hash)
   end
-
 end
